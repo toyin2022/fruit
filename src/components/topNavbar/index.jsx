be078d45b8da92b37d9e943bcbaca27134404648
toyin2022 <shevioni@yahoo.com>
2024-08-08 13:29:26 +0100

    image added

--- src/components/topNavbar/index.jsx
+++ src/components/topNavbar/index.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./topNavbar.css";
+import { MdLocationOn, MdMail } from "react-icons/md";
 
 const TopNavbar = () => {
-  return <div>TopNavbar</div>;
+  return (
+    <div className="end">
+      <div className="topNav">
+        <div className="leftTopNav">
+          <div className="leftText">
+            <MdLocationOn className="topNavIcons" />
+            <p> 123 Street, New York</p>
+          </div>
+          <div className="leftText">
+            <MdMail className="topNavIcons" />
+            <p>Email@Example.com</p>
+          </div>
+        </div>
+        <div className="rightTopNav">
+          <p>Privacy Policy/ Terms of Use/ Sales and Refunds</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TopNavbar;
