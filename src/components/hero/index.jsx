de41445475faba732ea6da87f8b1a28d1b583802
toyin2022 <shevioni@yahoo.com>
2024-08-08 15:17:47 +0100

    hero

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import "./Hero.css";
+import heroImg from "../../img/hero-img-1.png";
+import heroImg2 from "../../img/hero-img-2.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay, Scrollbar } from "swiper/modules"; // Correct import for modules
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div className="herocont">
+        <div className="heroText">
+          <p className="heroText1">100% Organic Foods</p>
+          <p className="heroText2">Organic Veggies & Fruits Foods</p>
+          <div className="searchBox">
+            <input type="text" className="searchInput" placeholder="Search" />
+            <button className="searchbtn">Submit Now</button>
+          </div>
+        </div>
+        <div className="hero-right">
+          <Swiper
+            modules={[Autoplay, Navigation, Pagination, Scrollbar]} // Register modules
+            // spaceBetween={50}
+            slidesPerView={1}
+            spaceBetween={50}
+            // scrollbar={{ draggable: true }}
+            autoplay={{ delay: 2000, disableOnInteraction: false }} // Configure Autoplay
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+          >
+            <SwiperSlide>
+              <div className="fruitslide">
+                <p className="fruitslideText">Fruits</p>
+              </div>
+              <img src={heroImg} alt="hero img" className="heroimg" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="fruitslide">
+                <p className="fruitslideText">Vesitable</p>
+              </div>
+              <img src={heroImg2} alt="hero img" className="heroimg" />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navCont">
+        <div className="">
+          <p className="fruitables">Fruitables</p>
+        </div>
+        <div className="menuicon">
+          <div className="greenLines"></div>
+          <div className="greenLines"></div>
+          <div className="greenLines"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
